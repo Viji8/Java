b32de07afa440adbcce0c712722ae87354d18ebd
Vijaya Lakshmi M <vijayalakshmiboopathi8@gmail.com>
2025-12-28 21:32:20 +0530

    Add files via upload

---
+++ while_pass_validation.java
@@ -0,0 +1,18 @@
+package assignments;
+import java.util.Scanner;
+
+public class while_pass_validation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter Your Passoword:");
+		Scanner pass=new Scanner(System.in);
+		String password=pass.next();
+		while(password.length()<8)
+		{
+			System.out.println("Password is too short");
+			password=pass.next();
+		}
+	}
+
+}
