1d67321581ee97ef102f77a35a2a501f4a04ee65
Vijaya Lakshmi M <vijayalakshmiboopathi8@gmail.com>
2026-01-04 19:49:04 +0530

    Update Static_Variable.java

--- Static_Variable.java
+++ Static_Variable.java
@@ -6,9 +6,11 @@ public class Static_Variable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Accessing Static Varibale using classname.fieldname=value;
 		Static_Variable.a="Bunny";
 		System.out.println("Access Static Variable using classname:  "+Static_Variable. a);
 
 	}
 
 }
+
